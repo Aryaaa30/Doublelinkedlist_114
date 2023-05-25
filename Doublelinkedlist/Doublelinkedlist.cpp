dea65af99e961acfe9e8e16035f113c741b8345c
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-05-25 19:38:26 +0700

    Membuat prosedur traverse yang berfungsi untuk mengunjungi element struktur dan membahasnya

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -102,6 +102,19 @@ bool DoubleLinkedList::listEmpty() {
     return (START == NULL);
 }
 
+void DoubleLinkedList::traverse() {
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else {
+        cout << "\nRecords in ascending order of roll number are :" << endl;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << "" << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
