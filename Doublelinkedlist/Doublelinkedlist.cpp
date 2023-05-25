ad39c17e579e962361eeee372401491190ec800c
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-05-25 15:36:36 +0700

    Membuat prosedur doublelinkedlist dan menambahkan data di dalamnya

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -22,6 +22,19 @@ public:
     void traverse(); 
 };
 
+DoubleLinkedList::DoubleLinkedList() {
+    START = NULL;
+}
+
+void DoubleLinkedList::addNode() {
+    int nim;
+    string nm;
+    cout << "\nEnter the roll number of the student :";
+    cin >> nim;
+    cout << "\nEnter the name of the student : ";
+    cin >> nm;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
